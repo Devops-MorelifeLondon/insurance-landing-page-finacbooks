ba6790fef6d428b10e551fe2f97cf55b448684e6
Devops-MorelifeLondon <devops@morelifelondon.net>
2025-11-04 16:10:32 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,17 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* ✅ Microsoft Clarity Tag */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u0pp3mjb38");
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
